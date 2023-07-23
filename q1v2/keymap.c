05df8313b905b83e0da99eccb097ece8eaceb1d3
Daniel Dirks <dandirks@web.de>
2023-07-23 20:02:43 +0200

    Use alt unicodes to overwrite dead keys

--- q1v2/keymap.c
+++ q1v2/keymap.c
@@ -26,8 +26,9 @@ enum layers{
 };
 
 enum custom_keycodes {
-    CU_GRV  = SAFE_RANGE,  // overwrites dead grave
-    CU_QUOT,               // overwrites dead quote
+    CU_6 = SAFE_RANGE,  // overwrites dead circumflex
+    CU_GRV,             // overwrites dead grave
+    CU_QUOT,            // overwrites dead quote
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -52,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_BASE] = LAYOUT_ansi_82(
         KC_ESC,          KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,       KC_F7,       KC_F8,       KC_F9,       KC_F10,      KC_F11,      KC_F12,      KC_DEL,                   KC_MUTE,
-        CU_GRV,          KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_MINS,     KC_EQL,      KC_BSPC,                  KC_SLEP,
+        CU_GRV,          KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        CU_6,        KC_7,        KC_8,        KC_9,        KC_0,        KC_MINS,     KC_EQL,      KC_BSPC,                  KC_SLEP,
         KC_TAB,          KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,        KC_LBRC,     KC_RBRC,     KC_BSLS,                  KC_PSCR,
         LT(3, KC_CAPS),  KC_A,        KC_S,        KC_D,        KC_F,        KC_G,        KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     CU_QUOT,                  KC_ENT,                   KC_SCRL,
         KC_LSFT,                      KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,                  KC_RSFT,     KC_UP,
@@ -84,16 +85,64 @@ void keyboard_post_init_user(void) {
     rgb_matrix_enable_noeeprom();
 }
 
+void send_key(uint16_t keycode) {
+    register_code(keycode);
+    unregister_code(keycode);
+}
+
+void send_keys_as_unicode(uint16_t keycode1, uint16_t keycode2, uint16_t keycode3) {
+    register_code(KC_LALT);
+    send_key(keycode1);
+    send_key(keycode2);
+    send_key(keycode3);
+    unregister_code(KC_LALT);
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        case CU_6:
+            if (record->event.pressed) {
+                uint8_t mods = get_mods();
+                clear_mods();
+
+                if (mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT) | MOD_BIT(KC_CAPS))) {
+                    send_keys_as_unicode(KC_P0, KC_P9, KC_P4);  // ^
+                } else {
+                    send_key(KC_6);
+                }
+
+                set_mods(mods);
+                return false;
+            }
+            break;
         case CU_GRV:
             if (record->event.pressed) {
-                    SEND_STRING("` ");
+                uint8_t mods = get_mods();
+                clear_mods();
+
+                if (mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT) | MOD_BIT(KC_CAPS))) {
+                    send_keys_as_unicode(KC_P1, KC_P2, KC_P6);  // ~
+                } else {
+                    send_keys_as_unicode(KC_P0, KC_P9, KC_P6);  // `
+                }
+
+                set_mods(mods);
+                return false;
             }
             break;
         case CU_QUOT:
             if (record->event.pressed) {
-                    SEND_STRING("' ");
+                uint8_t mods = get_mods();
+                clear_mods();
+
+                if (mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT) | MOD_BIT(KC_CAPS))) {
+                    send_keys_as_unicode(KC_P0, KC_P3, KC_P4);  // "
+                } else {
+                    send_keys_as_unicode(KC_P0, KC_P3, KC_P9);  // '
+                }
+
+                set_mods(mods);
+                return false;
             }
             break;
     }
