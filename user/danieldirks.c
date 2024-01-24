80f5c1ac57b4dce33e5936abf42f97efd179d1b1
Daniel Dirks <dandirks@web.de>
2024-01-24 22:41:50 +0100

    feat(user): add breathing rgb effect

--- user/danieldirks.c
+++ user/danieldirks.c
@@ -13,19 +13,5 @@ const uint32_t unicode_map[] PROGMEM = {
 };
 
 void keyboard_post_init_user(void) {
-    // rgb setup https://docs.qmk.fm/#/feature_rgb_matrix?id=functions
-    rgb_matrix_sethsv_noeeprom(0, 0, 0);
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    rgb_matrix_enable_noeeprom();
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    switch (get_highest_layer(state)) {
-        case _FN:
-            break;
-        default:
-            break;
-    }
-    return state;
+    rgb_matrix_mode(RGB_MATRIX_CUSTOM_BREATHING);
 }
