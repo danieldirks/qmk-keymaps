d0f59de41aa27b1987361450bdc38f294cd542d5
Daniel Dirks <dandirks@web.de>
2024-08-04 17:15:01 +0200

    feat(user): add unicode input detection based on os

--- user/danieldirks.c
+++ user/danieldirks.c
@@ -15,3 +15,24 @@ const uint32_t unicode_map[] PROGMEM = {
 void keyboard_post_init_user(void) {
     rgb_matrix_mode(RGB_MATRIX_CUSTOM_BREATHING);
 }
+
+bool process_detected_host_os_kb(os_variant_t detected_os) {
+    if (!process_detected_host_os_user(detected_os)) {
+        return false;
+    }
+    switch (detected_os) {
+        case OS_LINUX:
+            set_unicode_input_mode(UNICODE_MODE_LINUX);
+            break;
+        case OS_MACOS:
+            set_unicode_input_mode(UNICODE_MODE_MACOS);
+            break;
+        case OS_WINDOWS:
+            set_unicode_input_mode(UNICODE_MODE_WINCOMPOSE);
+            break;
+        case OS_IOS:
+        case OS_UNSURE:
+            break;
+    }
+    return true;
+}
