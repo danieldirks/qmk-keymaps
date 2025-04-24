821ff2faf230e2bb1e59a820e709ae57dbd28354
Daniel Dirks <dandirks@web.de>
2025-04-24 10:58:55 +0200

    feat(user): chnage to consistent 4 digit unicode points

--- user/danieldirks.c
+++ user/danieldirks.c
@@ -1,13 +1,13 @@
 #include "danieldirks.h"
 
 const uint32_t unicode_map[] PROGMEM = {
-    [UMLA]  = 0xE4,
-    [UMLAA] = 0xC4,
-    [UMLO]  = 0xF6,
-    [UMLOO] = 0xD6,
-    [UMLU]  = 0xFC,
-    [UMLUU] = 0xDC,
-    [UMLS]  = 0xDF,
+    [UMLA]  = 0x00E4,
+    [UMLAA] = 0x00C4,
+    [UMLO]  = 0x00F6,
+    [UMLOO] = 0x00D6,
+    [UMLU]  = 0x00FC,
+    [UMLUU] = 0x00DC,
+    [UMLS]  = 0x00DF,
     [UMLSS] = 0x1E9E,
     [EURO]  = 0x20AC,
 };
